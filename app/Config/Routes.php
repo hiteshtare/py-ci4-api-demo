871ce5d2d93471cf9327c459b05581474db74b2e
Hitesh Tare <hiteshtare@gmail.com>
2025-01-15 13:40:52 +0530

    Implemented routes->resource 'tasks' Successfully

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,9 +7,10 @@ use CodeIgniter\Router\RouteCollection;
  */
 // $routes->get('/', 'Home::index');
 
-$routes->post('/create','Tasks::create');
-$routes->post('/','Tasks::index');
-$routes->post('/show/(:num)','Tasks::show/$1');
-$routes->post('/update/(:num)','Tasks::update/$1');
-$routes->post('/delete/(:num)','Tasks::delete/$1');
-// $routes->resource('tasks');
+// $routes->post('/create','Tasks::create');
+// $routes->post('/','Tasks::index');
+// $routes->post('/show/(:num)','Tasks::show/$1');
+// $routes->post('/update/(:num)','Tasks::update/$1');
+// $routes->post('/delete/(:num)','Tasks::delete/$1');
+
+$routes->resource('tasks');
