57253f190c7b16f47254fd1ce5d0248da457dd7e
Hitesh Tare <hiteshtare@gmail.com>
2025-01-30 16:56:24 +0530

    Added new method - show

--- app/Controllers/Caleventsforwebsite.php
+++ app/Controllers/Caleventsforwebsite.php
@@ -9,6 +9,13 @@ use CodeIgniter\RESTful\ResourceController;
 
 class CalEventsForWebsite extends BaseController
 {
+    public $model;
+
+    public function __construct()
+    {
+        $this->model = new CalEventForWebsite();
+
+    }
     use ResponseTrait;
 
     public function index()
@@ -17,4 +24,46 @@ class CalEventsForWebsite extends BaseController
         $data = $model->findAll();
         return $this->respond($data);
     }
+
+    /**
+     * Return a new resource object, with default properties.
+     *
+     * @return ResponseInterface
+     */
+    public function show()
+    {
+        $search_data = [];
+
+        $start_date = $this->request->getVar('start_date');
+        $end_date = $this->request->getVar('end_date');
+
+        $error_msg = [];
+        $error_msg['reason'] = '';
+
+        if ($start_date > $end_date) {
+            $error_msg['reason'] .= "Start date cannot be greater than the end date";
+        }
+
+        $search_data = [
+            'start_date' => $start_date,
+            'end_date' => $end_date,
+        ];
+
+        $events = $this->model->getevents($search_data);
+        if (!empty($events)) {
+            $response = array('success' => true, 'events' => $events, "status" => 200);
+            // echo json_encode($response);
+            return $this->respond($response);
+        } else {
+            if (!$error_msg['reason'] == '') {
+                $error = $error_msg['reason'];
+            } else {
+                $error = "searching record not found on database";
+            }
+            $response = array('success' => false, "message" => $error, "status" => 204);
+            // echo json_encode($response);
+            return $this->respond($response);
+        }
+    }
+
 }
--- app/Models/CalEventForWebsite.php
+++ app/Models/CalEventForWebsite.php
@@ -6,7 +6,40 @@ use CodeIgniter\Model;
 
 class CalEventForWebsite extends Model
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     protected $table = 'calendar_event';
     protected $primaryKey = 'calendar_event_id';
-    protected $allowedFields = ['summary'];
+    protected $allowedFields = [
+        'end_date',
+        'location',
+        'latitude',
+        'event_type',
+        'language',
+        'longitude'
+    ];
+
+    function getevents($search_data)
+    {
+        $start_date = $search_data['start_date'];
+        $end_date = $search_data['end_date'];
+
+        $fstart = date('Y-m-d', strtotime($start_date));
+        $fend = date('Y-m-d', strtotime($end_date));
+
+        $where = '';
+        if ($end_date != '') {
+            $where .= " ce.dt_start >= '" . $fstart . "' AND ce.dt_end <= '" . $fend . "'";
+
+        } else {
+            $where .= "1";
+        }
+
+        $query = $this->db->query("SELECT * from calendar_event ce where $where");
+        return $query->getResultArray();
+    }
+
 }
