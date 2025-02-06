fbe87c6fd2f30c4b6d6ffee934bc3d05e1339d7f
Hitesh Tare <hiteshtare@gmail.com>
2025-02-06 15:37:24 +0530

    Added deep_unset function & created nested array struct

--- app/Controllers/Caleventsforwebsite.php
+++ app/Controllers/Caleventsforwebsite.php
@@ -52,11 +52,34 @@ class CalEventsForWebsite extends BaseController
         ];
 
         $events = $this->model->getevents($search_data);
-        if (!empty($events)) {
 
+        // To check if events is not empty
+        if (!empty($events)) {
+            // To group events by 'calender_id'
             $group_events = $this->group_by("calendar_id", $events);
 
-            $response = array('success' => true, 'events' => $group_events, "status" => 200);
+            $curated_events = array();
+
+            foreach ($events as $event) {
+                
+                // To remove redudant attributes from child grouped events
+                $this->deep_unset($group_events[$event["calendar_id"]], 'calendar_id');
+                $this->deep_unset($group_events[$event["calendar_id"]], 'xwr_calname');
+                $this->deep_unset($group_events[$event["calendar_id"]], 'xwr_timezone');
+
+                // To create parent child array based on key 'calendar_id'
+                $curated_events[] = array(
+                    'calendar_id' => $event["calendar_id"],
+                    'xwr_calname' => $event["xwr_calname"],
+                    'xwr_timezone' => $event["xwr_timezone"],
+                    'event_date' => $group_events[$event["calendar_id"]]
+                );
+            }
+
+            // To sort curated events using array unique
+            $curated_events = array_unique($curated_events, SORT_REGULAR);
+
+            $response = array('success' => true, 'events' => $curated_events, "status" => 200);
             // echo json_encode($response);
             return $this->respond($response);
         } else {
@@ -91,5 +114,27 @@ class CalEventsForWebsite extends BaseController
         }
 
         return $result;
+    }
+
+    /**
+     * Unsets array keys and object properties of the given name.
+     *
+     * @param array|object $data  An iterable object or array to modify.
+     * @param string       $field The name of the key or property to remove.
+     */
+    function deep_unset(array|object &$data, string $field)
+    {
+
+        if (is_array($data)) {
+            unset($data[$field]);
+        } elseif (is_object($data)) {
+            unset($data->{$field});
+        }
+
+        foreach ($data as &$value) {
+            if (is_array($value) || is_object($value)) {
+                $this->deep_unset($value, $field);
+            }
+        }
     }
 }
